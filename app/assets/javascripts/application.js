9b30dc49cd171d292e81ff51dc86d26b280cd126
tyomon <yomochitakuma@Maria.local>
2019-06-15 21:48:46 +0900

    コマの移動をできるようにした

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,32 +17,69 @@
 //= require jquery_ujs
 //= require_tree .
 
-let turn = 1;
+let turn = 1;               //ターンを表すフラグ 1:先攻 2:後攻
+let move_flg = 0;           //コマの移動中かどうかを表すフラグ
+var move_point_id = "";     //移動元のID
 
 $(function(){
   $('table td').click(function(){
+    //先攻のターン
     if(turn == 1){
-      if($(this).text() == "●"){
-        $(this).text(' ');      
-      }else{
-        $(this).text('●');
-      }  
+      //コマの移動中ではない
+      if(move_flg == 0){
+        if($(this).text() == "●"){
+          $(this).text('★');  
+          move_flg = 1;
+          move_point_id = '#' + $(this).attr('id');   //移動元IDを記録
+          console.log(move_point_id); 
+        }else if($(this).text() == ''){
+          $(this).text('●');
+        }  
+      //コマの移動中
+      }else if(move_flg == 1){
+        if($(this).text() == '★'){
+          $(this).text('●');
+          move_flg = 0;
+        }else if($(this).text() == ''){
+          $(this).text('●');
+          $(move_point_id).text('');
+          move_flg = 0;
+        }
+      }
+    //後攻のターン
     }else{
-      if($(this).text() == "○"){
-        $(this).text(' ');      
-      }else{
-        $(this).text('○');
-      }  
+      //コマの移動中ではない
+      if(move_flg == 0){
+        if($(this).text() == "○"){
+          $(this).text('★');  
+          move_flg = 1;
+          move_point_id = '#' + $(this).attr('id');   //移動元IDを記録
+          console.log(move_point_id); 
+        }else if($(this).text() == ''){
+          $(this).text('○');
+        }  
+      //コマの移動中
+      }else if(move_flg == 1){
+        if($(this).text() == '★'){
+          $(this).text('○');
+          move_flg = 0;
+        }else if($(this).text() == ''){
+          $(this).text('○');
+          $(move_point_id).text('');
+          move_flg = 0;
+        }
+      } 
     }
   });
 
+  //ターン終了ボタンのクリックイベント
   $('#turn-end').click(function(){
     turn = 3 - turn;
 
     if(turn == 1){
-      $("#current-turn").text('青のターン');
+      $('#current-turn').text('青のターン');
     }else{
-      $("#current-turn").text('白のターン');
+      $('#current-turn').text('白のターン');
     }
   });
 });
