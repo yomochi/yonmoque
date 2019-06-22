a0ba4619b972da19856c722742ef2a4b45470f5c
tyomon <yomochitakuma@Maria.local>
2019-06-22 21:01:08 +0900

    勝敗の判定をできるようにした

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,6 +26,8 @@ var row = 0;              //クリックしたテーブルの縦座標Y
 var col = 0;              //クリックしたテーブルの横座標X
 var rowBk = 0;            //Y座標のバックアップ
 var colBk = 0;            //X座標のバックアップ
+var winFlg = false;       //勝利判定
+var loseFlg = false;      //敗北判定
 
 /* 盤面の状態
 0:[中立マス] なし
@@ -55,100 +57,101 @@ $(function(){
     row = $(this).closest('tr').index();    //縦
     col = this.cellIndex;                   //横
 
-    //先攻のターン
-    if(turn == 1){
-      //コマの移動中
-      if(moveFlg){
-        if($(this).text() == '★'){
-          $(this).text('●');
-          putBan();
-          moveFlg = false;
-          turnChange();
-          return false;
-        }else if($(this).text() == ''){
-          if(judgMove()){
+    if(winFlg == loseFlg){
+      //先攻のターン
+      if(turn == 1){
+        //コマの移動中
+        if(moveFlg){
+          if($(this).text() == '★'){
             $(this).text('●');
-            $(movePointId).text('');
-            moveFlg = false;
             putBan();
-            reverse();
             turnChange();
-            return false;
+            moveFlg = false;
+          }else if($(this).text() == ''){
+            if(judgMove()){
+              $(this).text('●');
+              $(movePointId).text('');
+              putBan();
+              reverse();
+              if(winFlg == loseFlg){
+                turnChange();
+                moveFlg = false;
+              }
+            }else{
+              $('#msg').text('そこには置けません！！');
+            }
           }else{
             $('#msg').text('そこには置けません！！');
-            return false;
           }
+        //コマの移動中ではない
         }else{
-          $('#msg').text('そこには置けません！！');
-          return false;
-        }
-      //コマの移動中ではない
-      }else{
-        if($(this).text() == "●"){
-          $(this).text('★');
-          moveFlg = true;
-          movePointId = '#' + $(this).attr('id');   //移動元IDを記録
-          moveRecord();
-          pullBan();
-          return false;
-        }else if($(this).text() == ''){
-          if(blueStock > 0){
-            $(this).text('●');
-            stockCalc(--blueStock);
-            putBan();
-            turnChange();
-            return false;
+          if($(this).text() == "●"){
+            $(this).text('★');
+            movePointId = '#' + $(this).attr('id');   //移動元IDを記録
+            moveRecord();
+            pullBan();
+            moveFlg = true;
+          }else if($(this).text() == ''){
+            if(blueStock > 0){
+              $(this).text('●');
+              stockCalc(--blueStock);
+              putBan();
+              judgVictory(row, col);
+              if(winFlg == loseFlg){
+                turnChange();
+              }
+            }
           }
-          return false;
         }
-      }
-    //後攻のターン
-    }else{
-      //コマの移動中
-      if(moveFlg){
-        if($(this).text() == '★'){
-          $(this).text('○');
-          putBan();
-          moveFlg = false;
-          turnChange();
-          return false;
-        }else if($(this).text() == ''){
-          if(judgMove()){
+      //後攻のターン
+      }else{
+        //コマの移動中
+        if(moveFlg){
+          if($(this).text() == '★'){
             $(this).text('○');
-            $(movePointId).text('');
-            moveFlg = false;
             putBan();
-            reverse();
             turnChange();
-            return false;
+            moveFlg = false;
+          }else if($(this).text() == ''){
+            if(judgMove()){
+              $(this).text('○');
+              $(movePointId).text('');
+              putBan();
+              reverse();
+              if(winFlg == loseFlg){
+                turnChange();
+                moveFlg = false;
+              }
+            }else{
+              $('#msg').text('そこには置けません！！');
+            }
           }else{
             $('#msg').text('そこには置けません！！');
-            return false;
           }
+        //コマの移動中ではない
         }else{
-          $('#msg').text('そこには置けません！！');
-          return false;
-        }
-      //コマの移動中ではない
-      }else{
-        if($(this).text() == "○"){
-          $(this).text('★');
-          moveFlg = true;
-          movePointId = '#' + $(this).attr('id');   //移動元IDを記録
-          moveRecord();
-          pullBan();
-          return false;
-        }else if($(this).text() == ''){
-          if(whiteStock > 0){
-            $(this).text('○');
-            stockCalc(--whiteStock);
-            putBan();
-            turnChange();
-            return false;
+          if($(this).text() == "○"){
+            $(this).text('★');
+            movePointId = '#' + $(this).attr('id');   //移動元IDを記録
+            moveRecord();
+            pullBan();
+            moveFlg = true;
+          }else if($(this).text() == ''){
+            if(whiteStock > 0){
+              $(this).text('○');
+              stockCalc(--whiteStock);
+              putBan();
+              judgVictory(row, col);
+              if(winFlg == loseFlg){
+                turnChange();
+              }
+            }
           }
-          return false;
         }
       }
+    }
+    if(winFlg != loseFlg){
+      gameEnd();
     }
   });
 
@@ -244,6 +247,7 @@ $(function(){
   //コマの裏返し処理
   function reverse(){
     var ry,rx,dy,dx;
+    var resVictory = 0;
 
     for(dy = -1; dy <= 1; dy++){
       for(dx = -1; dx <= 1; dx++){
@@ -265,9 +269,15 @@ $(function(){
               if(ban[ry][rx] == 1 || ban[ry][rx] == 4 || ban[ry][rx] == 7){
                 $('#cell-' + ry + '' + rx).text('○');
                 ban[ry][rx] += 1;
+                resVictory = judgVictory(ry, rx);
               }else if(ban[ry][rx] == 2 || ban[ry][rx] == 5 || ban[ry][rx] == 8){
                 $('#cell-' + ry + '' + rx).text('●');
                 ban[ry][rx] -= 1;
+                resVictory = judgVictory(ry, rx);
+              }
+              //4目以上コマが並んでいたら
+              if(winFlg != loseFlg){
+                return false;
               }
               ry -= dy;
               rx -= dx;
@@ -275,6 +285,108 @@ $(function(){
             break;
           }
         }
+      }
+    }
+  }
+
+  //勝敗判定
+  function judgVictory(ry, rx){
+    var y = 0;
+    var x = 0;
+    var cnt = 0;
+
+    //縦軸を調べる
+    for(y = 1; y <= 5; y++){
+      if(turn == 1){
+        if(ban[y][rx] == 1 || ban[y][rx] == 4 || ban[y][rx] == 7){
+          cnt++;
+        }
+      }else{
+        if(ban[y][rx] == 2 || ban[y][rx] == 5 || ban[y][rx] == 8){
+          cnt++;
+        }
+      }
+    }
+    judgCnt(cnt);
+    cnt = 0;
+
+    //横軸を調べる
+    for(x = 0; x <= 5; x++){
+      if(turn == 1){
+        if(ban[ry][x] == 1 || ban[ry][x] == 4 || ban[ry][x] == 7){
+          cnt++;
+        }
+      }else{
+        if(ban[ry][x] == 2 || ban[ry][x] == 5 || ban[ry][x] == 8){
+          cnt++;
+        }
+      }
+    }
+    judgCnt(cnt);
+    cnt = 0;
+
+    //斜めを調べる
+    judgCnt(naname(1, 2, 1, 1, 4));
+    cnt = 0;
+    judgCnt(naname(1, 1, 1, 1, 5));
+    cnt = 0;
+    judgCnt(naname(2, 1, 1, 1, 4));
+    cnt = 0;
+    judgCnt(naname(1, 4, 1, -1, 4));
+    cnt = 0;
+    judgCnt(naname(1, 5, 1, -1, 5));
+    cnt = 0;
+    judgCnt(naname(2, 5, 1, -1, 4));
+  }
+
+  function naname(ry, rx, addy, addx, kaisu){
+    var i;
+    var cnt = 0;
+
+    for(i = 0; i < kaisu; i++){
+      if(turn == 1){
+        if(ban[ry][rx] == 1 || ban[ry][rx] == 4 || ban[ry][rx] == 7){
+          cnt++;
+        }
+      }else{
+        if(ban[ry][rx] == 2 || ban[ry][rx] == 5 || ban[ry][rx] == 8){
+          cnt++;
+        }
+      }
+      ry += addy;
+      rx += addx;
+    }
+    return cnt;
+  }
+  function judgCnt(cnt){
+    if(cnt == 5){
+      loseFlg = true;
+    }
+    if(cnt == 4){
+      winFlg = true;
+    }
+  }
+
+  function gameEnd(){
+    //コマをおいて４つ並んだ場合は無視する
+    if(moveFlg == false && winFlg){
+      winFlg = false;
+      turnChange();
+      return false;
+    }
+
+    $('#current-turn').text('');
+    if(turn == 1){
+      if(winFlg){
+        $('#msg').text('青の勝ち');
+      }else{
+        $('#msg').text('白の勝ち');
+      }
+    }else{
+      if(winFlg){
+        $('#msg').text('白の勝ち');
+      }else{
+        $('#msg').text('青の勝ち');
       }
     }
   }
